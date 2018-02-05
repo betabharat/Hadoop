22c9cb6b74647ba354b8b6e3db938ea4243d6e1f
Bharat <betabharat@gmail.com>
2018-02-05 23:43:09 +0100

    Correct use ofparent class

--- FirstProject/src/main/java/com/hadoop/wordcount/Reduce.java
+++ FirstProject/src/main/java/com/hadoop/wordcount/Reduce.java
@@ -1,26 +1,24 @@
 package com.hadoop.wordcount;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 	@Override
-	public void reduce(Text arg0, Iterator<IntWritable> arg1, OutputCollector<Text, IntWritable> arg2, Reporter arg3)
-			throws IOException {
-
+	protected void reduce(Text kez, Iterable<IntWritable> valColl,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
 		int sum = 0;
-		while (arg1.hasNext()) {
-			sum += arg1.next().get();
+		
+		for (IntWritable intWritable : valColl) {
+			sum += intWritable.get();
 		}
-		arg2.collect(arg0, new IntWritable(sum));
+		
+		context.write(kez, new IntWritable(sum));
 	}
+	
 
 }
